4b70142a33ec46596236a860f774b1bd59ab6d20
Seow Wen Han <wseow@seekasia.com>
2023-08-16 11:32:46 +0800

    Initial commit of bowling kata TDD

---
+++ bowling_game.js
@@ -0,0 +1,16 @@
+class BowlingGame {
+
+}
+
+module.exports = BowlingGame;
+
+// Prereq - Do you have npm and node installed?
+// At this point you probably should have them installed
+
+// 1 - npm init --yes
+// 2 - npm install --save-dev jest
+// 3 - Add jest into package.json's scripts.test
+// 4 - Try set up the barebone code and run npm test
+
+// JS Jest Setup
+// https://jestjs.io/docs/getting-started
---
+++ bowling_game.test.js
@@ -0,0 +1,33 @@
+const BowlingGame = require('./bowling_game');
+
+describe('Bowling Game', () => {
+  test('Should be defined', () => {
+    expect(new BowlingGame()).toBeDefined();
+  });
+
+  describe('Roll', () => {
+    // beforeAll(() => {
+    // });
+
+    // beforeEach(() => {
+    // });
+
+    test('Roll Normal', () => {
+
+    });
+
+    test('Roll Spare', () => {
+
+    });
+
+    test('Roll Strike', () => {
+
+    });
+
+    test('Roll Full Game', () => {
+
+    });
+  });
+});
+
+// Jest Matchers - https://jestjs.io/docs/using-matchers
